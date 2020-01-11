787cc9370f06011771dbfb1743ba344e8befabfd
Jakexxh <jakewill0922@gmail.com>
2020-01-11 12:47:16 -0500

    for fixed

--- data/data_generator.py
+++ data/data_generator.py
@@ -1,16 +1,29 @@
 import tensorflow as tf
-import pathlib
-import matplotlib.pyplot as plt
+import scipy.stats as stats
 import util.util_funs as funs
 import random
 import numpy as np
-from main import SGD_p
+# from main import SGD_p
+SGD_p = {
+    'lr': 0.1,  # TODO: origin is 0.01
+    'max_grad_norm': 1,
+    'vanish_grad_reg': 2,
+    'tau': 100,
+    'train_t_step': 20,
+    'test_t_step': 0.5,
+    'ini_spe_r': 1.5,
+    'minibatch_size': 20,
+    'baseline_input': 0.2,
+    'input_noise_std': 0.01,
+    'rr_noise_std': 0.15,
+    'mini_w_threshold': 10 ** -6  # TODO: origin is 10**-4
+}
 import sys
 
 np.set_printoptions(precision=5)
 RT_FIX_T_MEAN = 200 #700
 RT_REWARD_DELAY_T = 300
-TRIAL_T = 3000  # TODO: may change
+TRIAL_T = 2000  # TODO: may change
 LOW_VALUE = 0.2
 HIGH_VALUE = 1.2
 REACTION_THRESHOLD = 1.0
@@ -27,51 +40,64 @@ class DataGenerator:
         self.ct_portion = 0.1  # self.p.ct_portion
         self.batch_size = SGD_p['minibatch_size']
 
+
         if task_version == 'rt':
 
             self.single_trial_fun = self.single_rt_train_trial
 
             if action == 'train':
                 self.dt = SGD_p['train_t_step']
-                self.trial_len = int(TRIAL_T / self.dt)
+                self.trial_len = TRIAL_T // self.dt
                 self.alpha = SGD_p['train_t_step'] / SGD_p['tau']
-                self.fix_t = 100 # todo: self.rt_mk_fix_t()
+                self.fix_t = 100  # todo: may change
                 self.step_flag = {
-                    'fixation': (0, int(self.fix_t / self.dt)),
-                    'stimulus': (int(self.fix_t / self.dt), self.trial_len),
-                    'decision': (int((self.fix_t + RT_REWARD_DELAY_T) / self.dt), self.trial_len)}
+                    'fixation': (0, self.fix_t // self.dt),
+                    'stimulus': (self.fix_t // self.dt, self.trial_len),
+                    'decision': ((self.fix_t + RT_REWARD_DELAY_T) // self.dt, self.trial_len)}
 
             else:
                 self.dt = SGD_p['test_t_step']
-                self.trial_len = int(TRIAL_T / self.dt)
+                self.trial_len = TRIAL_T // self.dt
                 self.alpha = SGD_p['test_t_step'] / SGD_p['tau']
-                self.fix_t = 100  # todo: self.rt_mk_fix_t()
+                self.fix_t = 300  # todo: may change
                 self.step_flag = {
-                    'fixation': (0, int(self.fix_t / self.dt)),
-                    'stimulus': (int(self.fix_t / self.dt), self.trial_len),
-                    'decision': (int((self.fix_t + RT_REWARD_DELAY_T) / self.dt), self.trial_len)}
-
+                    'fixation': (0, self.fix_t // self.dt),
+                    'stimulus': (self.fix_t // self.dt, self.trial_len),
+                    'decision': ((self.fix_t + RT_REWARD_DELAY_T) // self.dt, self.trial_len)}
 
         elif task_version == 'fd':
+
+            self.single_trial_fun = self.single_fd_train_trial
+            self.truncated_norm = self.get_truncated_norm(80, 1500, 330)
+
             if action == 'train':
-                self.single_trial_fun = self.single_rt_train_trial
+                self.dt = SGD_p['train_t_step']
+                self.trial_len = TRIAL_T // self.dt
+                self.alpha = SGD_p['train_t_step'] / SGD_p['tau']
+                self.fix_t = 100  # todo: may change
+                self.step_flag = {
+                    'fixation': (0, self.fix_t // self.dt)}
             else:
-                pass
+                self.dt = SGD_p['test_t_step']
+                self.trial_len = TRIAL_T // self.dt
+                self.alpha = SGD_p['test_t_step'] / SGD_p['tau']
+                self.fix_t = 500  # todo: may change
+                self.step_flag = {
+                    'fixation': (0, self.fix_t // self.dt)}
 
         else:
             raise Exception('No task version: ' + task_version)
 
     def single_rt_train_trial(self):
-        """
 
-        :return: desc of trial, masks, inputs, outputs
-        """
         choice = 1 - random.choice(CHOICE_MAP)
         coh = random.choice(self.cohs)
 
         inputs = np.zeros((2, self.trial_len))
         outputs = np.zeros((2, self.trial_len))
         masks = np.ones(self.trial_len)
+
+        self.truncated_norm = self.get_truncated_norm()
 
         for step in range(self.trial_len):
 
@@ -92,6 +118,39 @@ class DataGenerator:
 
         return {'choice': choice, 'coh': coh},  masks, inputs.T, outputs
 
+    def single_fd_train_trial(self):
+
+        stim_t = self.truncated_norm.rvs(1)[0] // self.dt
+
+        self.step_flag['stimulus'] = (self.step_flag['fixation'][1], self.step_flag['fixation'][1] + stim_t)
+        self.step_flag['decision'] = (self.step_flag['fixation'][1] + stim_t, self.trial_len)
+
+        choice = 1 - random.choice(CHOICE_MAP)
+        coh = random.choice(self.cohs)
+
+        inputs = np.zeros((2, self.trial_len))
+        outputs = np.zeros((2, self.trial_len))
+        masks = np.ones(self.trial_len)
+
+        for step in range(self.trial_len):
+
+            if step < self.step_flag['fixation'][1]:
+                outputs[:, step] = LOW_VALUE
+                masks[step] = 1
+
+            if self.step_flag['stimulus'][0] <= step < self.step_flag['stimulus'][1]:
+                inputs[choice][step] = funs.rectify(SGD_p['baseline_input'] +
+                                                    self.stim_value(coh) + self.input_noise())
+                inputs[1 - choice][step] = funs.rectify(SGD_p['baseline_input'] +
+                                                        self.stim_value(-coh) + self.input_noise())
+
+            if step >= self.step_flag['decision'][0]:
+                outputs[choice][step] = HIGH_VALUE
+                outputs[1 - choice][step] = LOW_VALUE
+                masks[step] = 1
+
+        return {'choice': choice, 'coh': coh}, masks, inputs.T, outputs
+
     def single_catch_trial(self):
         inputs = np.zeros((2, self.trial_len))
         outputs = np.ones((2, self.trial_len)) * LOW_VALUE
@@ -99,18 +158,11 @@ class DataGenerator:
 
         return 'catch_trial', masks, inputs.T, outputs
 
-    def single_fd_train_trial(self):
-        pass
-
-    def rt_mk_fix_t(self, truncate=300):
-        def exp_fun(): return np.random.exponential(RT_FIX_T_MEAN) // SGD_p['train_t_step'] * SGD_p['train_t_step']
-
-        fix_t = exp_fun()
-
-        while not (RT_FIX_T_MEAN - truncate <= fix_t <= RT_FIX_T_MEAN + truncate):
-            fix_t = exp_fun()
-
-        return fix_t
+    @staticmethod
+    def get_truncated_norm(lower, upper, mean, scale=1.0):
+        trunc_norm = stats.truncnorm(
+            (lower - mean) / scale, (upper - mean) / scale, loc=mean, scale=scale)
+        return trunc_norm
 
     def make_data(self):
         pass
@@ -191,3 +243,12 @@ for m, inputs, outputs in zip(a[1], a[2], a[2]):
     
 """
 
+dg = DataGenerator(task_version='fd')
+a = next(dg)
+
+for m, inputs, outputs in zip(a[1], a[2], a[2]):
+    print(m)
+    i = inputs.numpy()
+    print(i)
+    o = outputs.numpy()
+    print(o)
